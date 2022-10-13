0beb9ec18d4ef79c6034056115ab47175c6dd840
Sky00l <207410290@gms.tku.edu.tw>
2022-10-13 21:26:59 +0800

    W06-P3: run 4 classdemo one by one

---
+++ js/demo_90.js
@@ -0,0 +1,18 @@
+function change(index) {
+    const p = document.querySelector('.show-classdemo');
+    console.log('p',p);
+    switch(index) {
+        case 1:
+            p.innerHTML = `<iframe src="./demo/w01_90/index.html" width="100%" height="100%" />`
+            break;
+        case 2:
+            p.innerHTML = `<iframe src="./demo/w02_90/card_90.html" width="100%" height="100%" />`
+            break;
+        case 3:
+            p.innerHTML = `<iframe src="./demo/w04_90/blog_90.html" width="100%" height="100%" />`
+            break;
+        case 4:
+            p.innerHTML = `<iframe src="./demo/w06_90/blog_90.html" width="100%" height="100%" />`
+            break;   
+    }
+}
