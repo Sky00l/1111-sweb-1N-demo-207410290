7e67305b2fe93f89a16f15048cf68d7156f3525b
Sky00l <207410290@gms.tku.edu.tw>
2022-10-27 19:00:08 +0800

    w08-P1: navbar with card1 and card2

---
+++ demo/w08_90/quiz1_90.js
@@ -0,0 +1,12 @@
+function showPage(index) {
+    const p = document.querySelector('.show-quiz1-answer');
+    console.log('p',p);
+    switch(index) {
+        case 1:
+            p.innerHTML = `<iframe src="./cards/card_90.html" width="100%" height="100%" />`
+            break;
+        case 2:
+            p.innerHTML = `<iframe src="./cards/card2_90.html" width="100%" height="100%" />`
+            break;  
+    }
+}
