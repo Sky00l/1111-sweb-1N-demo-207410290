adc00ee5c6eb6d3dc5b50aed8e67c4aec9ddaa69
Sky00l <207410290@gms.tku.edu.tw>
2022-12-28 11:01:22 +0800

    create resume files and connect to index.html

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ function change(index) {
           p.innerHTML = `<iframe src="./demo/works/work.html" width="100%" height="100%" />`
           break;
       case 3:
-          p.innerHTML = `<iframe src="./resume.html" width="100%" height="100%" />`
+          p.innerHTML = `<iframe src="./demo/resume/resume.html" width="100%" height="100%" />`
           break;
       case 4:
           p.innerHTML = `<iframe src="./learn_90.html" width="100%" height="100%" />`
