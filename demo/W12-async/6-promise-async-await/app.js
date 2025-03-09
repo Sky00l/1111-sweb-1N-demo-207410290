0bf92f574470740efb9cd2c07f8328545d555ee1
Sky00l <207410290@gms.tku.edu.tw>
2025-03-09 15:28:36 +0800

    create new navbar and edit index.html

---
+++ demo/W12-async/6-promise-async-await/app.js
@@ -0,0 +1,68 @@
+// callbacks, promise, async/await
+// What if no resolve, one is rejected
+const heading1 = document.querySelector('.one');
+const heading2 = document.querySelector('.two');
+const heading3 = document.querySelector('.three');
+const heading4 = document.querySelector('.four');
+
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', async () => {
+  const result = await disPlayColor();
+  console.log('result', result);
+});
+
+const disPlayColor = async () => {
+  try {
+    await addColor(1000, heading1, 'red');
+    await addColor(2000, heading2, 'green');
+    await addColor(1000, heading3, 'blue');
+    await addColor(500, heading4, 'pink');
+    console.log('success');
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+function addColor (time, element, color)  {
+  return new Promise((resolve, reject) => {
+    if (element) {
+      setTimeout(() => {
+        element.style.color = color;
+        resolve();
+      }, time);
+    } else {
+      reject(new Error(`There is no such element ${element}`));
+    }
+  });
+};
+
+// btn.addEventListener('click', () => {
+//   setTimeout(() => {
+//     heading1.style.color = 'red';
+//     setTimeout(() => {
+//       heading2.style.color = 'green';
+//       setTimeout(() => {
+//         heading3.style.color = 'blue';
+//         setTimeout(() => {
+//           heading4.style.color = 'pink';
+//         }, 500);
+//       }, 1000);
+//     }, 2000);
+//   }, 1000);
+// });
+
+const promise = new Promise((resolve, reject) => {
+  let value = true;
+  if (value) {
+    resolve([1, 2, 3, 4]);
+  } else {
+    reject('There was an error, value is false');
+  }
+});
+
+promise.then((data) => {
+  console.log('data', data);
+}).catch((error) => {
+  console.log(error);
+});
--- js/index.js
+++ js/index.js
@@ -1,19 +1,55 @@
-function change(index) {
-  const p = document.querySelector('.show-classdemo');
-  console.log('p',p);
-  switch(index) {
-      case 1:
-          p.innerHTML = `<iframe src="./demo_90.html" width="100%" height="100%" />`
-          break;
-      case 2:
-          p.innerHTML = `<iframe src="./demo/works/work.html" width="100%" height="100%" />`
-          break;
-      case 3:
-          p.innerHTML = `<iframe src="./demo/resume/resume.html" width="100%" height="100%" />`
-          break;
-      case 4:
-          p.innerHTML = `<iframe src="./demo/learn/learn_90.html" width="100%" height="100%" />`
-          break;
-     
-  }
+const showClassDemo = index => {
+    const showDemo = document.querySelector('.show-classdemo')
+    console.log('showDemo', showDemo)
+    switch (index) {
+        case 1:
+            showDemo.innerHTML = `<iframe src="./demo_90.html" width="100%" height="100%" />`
+            break
+        case 2:
+            showDemo.innerHTML = `<iframe src="./demo/works/work.html" width="100%" height="100%" />`
+            break
+        case 3:
+            showDemo.innerHTML = `<iframe src="./demo/resume/resume.html" width="100%" height="100%" />`
+            break
+        case 4:
+            showDemo.innerHTML = `<iframe src="./demo/learn/learn_90.html" width="100%" height="100%" />`
+            break
+        case 5:
+            showDemo.innerHTML = `<iframe src="./demo/w01_90/index.html" width="100%" height="100%" />`
+            break
+        case 6:
+            showDemo.innerHTML = `<iframe src="./demo/w02_90/card_90.html" width="100%" height="100%" />`
+            break
+        case 7:
+            showDemo.innerHTML = `<iframe src="./demo/w04_90/blog_90.html" width="100%" height="100%" />`
+            break
+        case 8:
+            showDemo.innerHTML = `<iframe src="./demo/w06_90/blog_90.html" width="100%" height="100%" />`
+            break
+        case 9:
+            showDemo.innerHTML = `<iframe src="./demo/w10_90/multipage_90.html" width="100%" height="100%" />`
+            break
+        case 10:
+            showDemo.innerHTML = `<iframe src="./demo/w02-imgGallery/imgGallery.html" width="100%" height="100%" />`
+            break
+        case 11:
+            showDemo.innerHTML = `<iframe src="./demo/w12_90_portfolio/portfolio/index.html" width="100%" height="100%" />`
+            break
+        case 12:
+            showDemo.innerHTML = `<iframe src="./demo/w13_90_portfolio/portfolio/index.html" width="100%" height="100%" />`
+            break
+        case 13:
+            showDemo.innerHTML = `<iframe src="./demo/w14_90_portfolio/portfolio/index.html" width="100%" height="100%" />`
+            break
+        case 14:
+            showDemo.innerHTML = `<iframe src="./demo/w11_90_midterm/p4_90/p4_90.html" width="100%" height="100%" />`
+            break
+        case 16:
+            showDemo.innerHTML = `<iframe src="./demo/w16_90_portfolio/portfolio/index.html" width="100%" height="100%" />`
+            break
+        case 15:
+            showDemo.innerHTML = `<iframe src="./demo/w07-ms-clone/index.html" width="100%" height="100%" />`
+            break
+
+    }
 }
